083ebf8e630b832e07a34033ede5cd0e3209bebe
Sean Chen <brand.identity.brand@google.com>
2023-03-15 20:12:42 -0400

    middleware applies GA4 params to /portal?id=0

--- middleware.js
+++ middleware.js
@@ -2,9 +2,9 @@ import { NextResponse } from 'next/server'
 
 export function middleware(req) {
     console.log(req.nextUrl)
-//   if ( req.nextUrl.pathname === '/portal?id=0' || req.nextUrl.pathname.startsWith('/portal/') ) {
-//     // https://blrow.world?utm_source=painting&utm_medium=qr&utm_campaign=alpha&utm_id=0
-//     console.log('middleware')
-//     return NextResponse.rewrite(new URL('/portal?id=2', req.url))
-//   }
+  if ( req.nextUrl.pathname === '/portal?id=0' || req.nextUrl.pathname.startsWith('/portal/') ) {
+    // https://blrow.world?utm_source=painting&utm_medium=qr&utm_campaign=alpha&utm_id=0
+    console.log('middleware')
+    return NextResponse.rewrite(new URL('/portal?id=0&utm_source=painting&utm_medium=qr&utm_campaign=alpha&utm_id=0', req.url))
+  }
 }
