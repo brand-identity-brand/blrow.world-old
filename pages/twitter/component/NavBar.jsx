943087bc7eacc927346435472ec25c088169d4e0
Sean Chen <brand.identity.brand@google.com>
2023-03-21 21:15:57 -0400

    fixed router import

--- pages/twitter/component/NavBar.jsx
+++ pages/twitter/component/NavBar.jsx
@@ -1,9 +1,11 @@
 import css from './NavBar.module.css';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
 import GogglesIcon from '../public/GogglesIcon.png';
 
 export default function NavBar(props){
+    const router = useRouter();
     return (
         <div className={css.navBarContainer}>
             <div className={css.iconContainer}
