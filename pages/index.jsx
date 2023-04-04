a2e2797282fb55e0bccde84e12c18bfb2c9657e6
brand-identity-brand <88216342+brand-identity-brand@users.noreply.github.com>
2023-04-04 11:18:55 -0400

    updated index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,11 @@
+import css from './index.module.css'
 import Timer from '@/component/Timer'
 import Head from 'next/head'
-// import Image from 'next/image'
+import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import Link from 'next/link'
-
+import Portal from '@/public/index/portal.png';
+import Crowd from '@/public/index/crowd.png';
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -16,9 +18,27 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Link href={'/portal'}> portal </Link>
-        <Timer/>
+      <main className={css.master}>
+        <div className={css.portalContainer}>
+          <Image
+            alt={'portal'}
+            src={Portal}
+            fill
+            style={{
+              objectFit: 'contain'
+            }}
+          />
+        </div>
+        <div className={css.crowdContainer}>
+          <Image
+            alt={'crowd'}
+            src={Crowd}
+            fill
+            style={{
+              objectFit: 'contain',
+            }}
+          />
+        </div>
       </main>
     </>
   )
