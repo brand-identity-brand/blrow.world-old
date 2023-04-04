168f0acdec29eb0baca842b5c68d78c3e2d4bcdd
brand-identity-brand <88216342+brand-identity-brand@users.noreply.github.com>
2023-04-03 22:01:05 -0400

    updated invitaion design

--- pages/youtube/index.jsx
+++ pages/youtube/index.jsx
@@ -4,19 +4,26 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 import Timer from '@/component/Timer'
-import { useContext, useEffect, useRef } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { TimerContext } from '@/context/TimerContext';
 import { ProgressContext } from '@/context/ProgressContext';
 import QRCodeRickRoll from '@/public/youtube/qrCode.png';
+import Frame from '@/public/youtube/frame.png';
+import { getCookie } from 'cookies-next';
 
 export default function Youtube() {
   const router = useRouter();
+  const [ artistCookie, setArtistCookie ] = useState('');
   const { progressState, pathUnlocked, stageVisited, isExhibitionReady } = useContext(ProgressContext);
   const { speed, visits } = progressState[5];
   const { TimerState, setTimerState } = useContext(TimerContext);
   const { timeLimit } = TimerState;
+
+  const [ titleEditing, setTitleEditing ] = useState(false);
+  const titleRef= useRef();
   useEffect(()=>{
     stageVisited(5);
+    setArtistCookie( getCookie('artist') );
     console.log(isExhibitionReady())
   },[]);
 
@@ -31,42 +38,78 @@ export default function Youtube() {
     </Head>
     <main className={css.main}>
       <Timer speed={speed}/>
-      <div 
-        className={css.qrCodeContainer}
-        onClick={()=>{
-          if ( exhibitionProgress.reduce( (accum,curr) => accum&&curr, true ) ) {
-            const lockProgress = confirm('start your exhibition?');
-            if ( lockProgress ) {
-              setTimerState({
-                timeLimit: timeLimit,
-                speed: 2147483647
-              });
-              pathUnlocked(5, 'blue');
-              router.push('/twitter');
-            }
-          } else if ( 
-            exhibitionProgress[1] === false ||
-            exhibitionProgress[2] === false ||
-            exhibitionProgress[3] === false ||
-            exhibitionProgress[4] === false
-          ) {
-            alert(`
-              set 1 ${exhibitionProgress[1]}
-              set 2 ${exhibitionProgress[2]}
-              set 3 ${exhibitionProgress[3]}
-              set 4 ${exhibitionProgress[4]}
-            `);
-          } else if ( exhibitionProgress[5]  === false ) {
-            alert('you need a title and a brief statement for your exhibition on the invitation.');
-          }
-        }}
-      >
-        <Image
-          alt={'QR Code'}
-          src={QRCodeRickRoll}
-          fill
-        />
+      <div className={css.headerContainer}>
+        <div className={css.textHeader}> You're invited to</div>
+        <div 
+          ref = {titleRef}
+          contentEditable={true}
+          suppressContentEditableWarning={true}
+          className={titleEditing? css.titleInput : css.title} 
+          onFocus={()=>{setTitleEditing(true)}}
+          onBlur={()=>{
+            setTitleEditing(false)
+          }}
+        >
+          Click here to title the exhibition
+        </div>
       </div>
+      <div className={css.bodyContainer}>
+        <div className={css.paintingContainer}>
+          <div className={css.frameContainer}>
+            <Image
+              alt={'frame'}
+              src={Frame}
+              fill
+            />
+          </div>
+          <div 
+            className={css.qrCodeContainer}
+            onClick={()=>{
+              if ( exhibitionProgress.reduce( (accum,curr) => accum&&curr, true ) ) {
+                const lockProgress = confirm('start your exhibition?');
+                if ( lockProgress ) {
+                  setTimerState({
+                    timeLimit: timeLimit,
+                    speed: 2147483647
+                  });
+                  pathUnlocked(5, 'blue');
+                  router.push('/twitter');
+                }
+              } else if ( 
+                exhibitionProgress[1] === false ||
+                exhibitionProgress[2] === false ||
+                exhibitionProgress[3] === false ||
+                exhibitionProgress[4] === false
+              ) {
+                alert(`
+                  set 1 ${exhibitionProgress[1]}
+                  set 2 ${exhibitionProgress[2]}
+                  set 3 ${exhibitionProgress[3]}
+                  set 4 ${exhibitionProgress[4]}
+                `);
+              } else if ( exhibitionProgress[5]  === false ) {
+                alert('you need a title and a brief statement for your exhibition on the invitation.');
+              }
+            }}
+          >
+            <Image
+              alt={'QR Code'}
+              src={QRCodeRickRoll}
+              fill
+            />
+          </div>
+        </div>
+        <div className={css.textPlayer}> 
+          <div > {'an exhibition of'} </div>
+          <div >{`#${artistCookie}`}</div>
+        </div>
+        
+      </div>
+
+      <div className={css.footerContainer}>
+        
+      </div>
+
     </main>
   </>)
 }
