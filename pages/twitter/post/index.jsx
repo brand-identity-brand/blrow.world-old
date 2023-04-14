58b3f60bf1e7404065d5f938ebdd45c2f4763c6c
Sean Chen <brand.identity.brand@gmail.com>
2023-04-13 22:41:20 -0400

    replace cookie with playerState in post

--- pages/twitter/post/index.jsx
+++ pages/twitter/post/index.jsx
@@ -1,155 +1,173 @@
-import Head from 'next/head'
-import css from './index.module.css'
+import Head from "next/head";
+import css from "./index.module.css";
 
-import { useRouter } from 'next/router'
-import Image from 'next/image'
-import TimerBar from '@/component/TimerBar';
-import { useContext, useState, useRef, useEffect } from 'react';
-import { TimerContext } from '@/context/TimerContext';
-import { ProgressContext } from '@/context/ProgressContext';
-import { PlayerContext } from '@/context/PlayerContext'
-import { getCookie } from 'cookies-next';
-import { api_art_title, api_art_statement } from '@/lib/fetcher';
+import { useRouter } from "next/router";
+import Image from "next/image";
+import TimerBar from "@/component/TimerBar";
+import { useContext, useState, useRef, useEffect } from "react";
+import { TimerContext } from "@/context/TimerContext";
+import { ProgressContext } from "@/context/ProgressContext";
+import { PlayerContext } from "@/context/PlayerContext";
+// import { getCookie } from 'cookies-next';
+import { api_art_title, api_art_statement } from "@/lib/fetcher";
 
-import { imageSrc, counter } from '@/lib/art';
-import useSaveScore from '@/hook/useSaveScore';
-import useSaveProgress from '@/hook/useSaveProgress'
+import { imageSrc, counter } from "@/lib/art";
+import useSaveScore from "@/hook/useSaveScore";
+import useSaveProgress from "@/hook/useSaveProgress";
 
-export default function Post(props){
-    const {
-        stage,
-        art
-    } = props;
-// console.log('timezone', Intl.DateTimeFormat().resolvedOptions().timeZone)
-    const { isStageVisited, getArtTitle, getArtStatement, setArtTitle, setArtStatement } = useContext(ProgressContext);
-    const router = useRouter();
+export default function Post(props) {
+  const { stage, art } = props;
+  // console.log('timezone', Intl.DateTimeFormat().resolvedOptions().timeZone)
+  const {
+    isStageVisited,
+    getArtTitle,
+    getArtStatement,
+    setArtTitle,
+    setArtStatement,
+  } = useContext(ProgressContext);
+  const router = useRouter();
 
-    if ( isStageVisited(stage) === false ) {
-        return (<>
-            <Head>
-                <title>blrow.world</title>
-                <meta name="description" content="blrow.world" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main className={css.main}>
-                <TimerBar speed={100} router={router}/>
-                <div className={css.a}>
-                    {'art missing.'}
-                </div>
-            </main>
-        </>)
-    }
-
-    
-    const { playerState } = useContext(PlayerContext);
-    const { progressState } = useContext(ProgressContext);
-    const { TimerState, setTimerState } = useContext(TimerContext);
-    const { timeLimit, speed } = TimerState;
-
-    const [titleEditing, setTitleEditing] = useState(false);
-    const [statementEditing, setStatementEditing] = useState(false);
-
-    const titleRef = useRef();
-    const statementRef = useRef();
-
-    useSaveScore( { playerState, timeLimit }, router );
-    useSaveProgress( { playerState, progressState }, router );
-
-    return (<>
+  if (isStageVisited(stage) === false) {
+    return (
+      <>
         <Head>
-            <title>blrow.world</title>
-            <meta name="description" content="blrow.world" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/favicon.ico" />
+          <title>blrow.world</title>
+          <meta name="description" content="blrow.world" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
         </Head>
         <main className={css.main}>
-            <TimerBar speed={speed} router={router}/>
-            <div className={css.a}>
-                <div className={css.imageContainer}>
-                    <Image
-                        src = { imageSrc[ counter(stage,art) ] }
-                        alt = { stage }
-                        fill
-                        style = {{
-                            objectFit: 'contain'
-                        }}
-                    />
-                </div>
-            </div>
-            <div className={css.b}>
-                <div 
-                    ref = {titleRef}
-                    contentEditable={true}
-                    suppressContentEditableWarning={true}
-                    className={titleEditing? css.titleInput : css.title} 
-                    onFocus={()=>{setTitleEditing(true)}}
-                    onBlur={()=>{
-                        setTitleEditing(false);
-                        if ( getArtTitle(stage, art) === titleRef.current.innerHTML.replace(/^\s+|\s+$/g, '') ){
-                            setTimerState ({
-                                timeLimit: timeLimit + 60,
-                                speed: speed - 200
-                            });
-                        } else {
-                            // TODO: check for duplicated title withint the same account before uploading
-                            api_art_title({ 
-                                player: getCookie('artist'),
-                                stage: stage, 
-                                art: art, 
-                                title: titleRef.current.innerHTML.replace(/^\s+|\s+$/g, '') 
-                            });
-                            setArtTitle(stage, art, titleRef.current.innerHTML.replace(/^\s+|\s+$/g, ''));
-                            setTimerState ({
-                                timeLimit: timeLimit + 60,
-                                speed: speed 
-                            });
-                            
-                        }
-                    }}
-                > 
-                    { getArtTitle(stage, art) }
-                </div>
-                <div
-                    ref={statementRef}
-                    contentEditable={true}
-                    suppressContentEditableWarning={true}
-                    className={statementEditing? css.statementInput : css.statement} 
-                    onFocus={()=>{setStatementEditing(true)}}
-                    onBlur={()=>{
-                        setStatementEditing(false);
-                        if ( getArtStatement(stage, art) === statementRef.current.innerHTML ){
-                            setTimerState ({
-                                timeLimit: timeLimit + 600,
-                                speed: speed - 200
-                            });
-                        } else {
-                            api_art_statement({ 
-                                player: getCookie('artist'),
-                                stage: stage, 
-                                art: art, 
-                                statement: statementRef.current.innerHTML
-                            });
-                            setArtStatement(stage, art, statementRef.current.innerHTML);
-                            setTimerState ({
-                                timeLimit: timeLimit + 600,
-                                speed: speed
-                            });
-                        }
-                    }}
-                    dangerouslySetInnerHTML={{ __html: getArtStatement(stage, art) }}
-                />
-            </div>
+          <TimerBar speed={100} router={router} />
+          <div className={css.a}>{"art missing."}</div>
         </main>
-    </>)
+      </>
+    );
+  }
+
+  const { playerState } = useContext(PlayerContext);
+  const { progressState } = useContext(ProgressContext);
+  const { TimerState, setTimerState } = useContext(TimerContext);
+  const { timeLimit, speed } = TimerState;
+
+  const [titleEditing, setTitleEditing] = useState(false);
+  const [statementEditing, setStatementEditing] = useState(false);
+
+  const titleRef = useRef();
+  const statementRef = useRef();
+
+  useSaveScore({ playerState, timeLimit }, router);
+  useSaveProgress({ playerState, progressState }, router);
+
+  return (
+    <>
+      <Head>
+        <title>blrow.world</title>
+        <meta name="description" content="blrow.world" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={css.main}>
+        <TimerBar speed={speed} router={router} />
+        <div className={css.a}>
+          <div className={css.imageContainer}>
+            <Image
+              src={imageSrc[counter(stage, art)]}
+              alt={stage}
+              fill
+              style={{
+                objectFit: "contain",
+              }}
+            />
+          </div>
+        </div>
+        <div className={css.b}>
+          <div
+            ref={titleRef}
+            contentEditable={true}
+            suppressContentEditableWarning={true}
+            className={titleEditing ? css.titleInput : css.title}
+            onFocus={() => {
+              setTitleEditing(true);
+            }}
+            onBlur={() => {
+              setTitleEditing(false);
+              if (
+                getArtTitle(stage, art) ===
+                titleRef.current.innerHTML.replace(/^\s+|\s+$/g, "")
+              ) {
+                setTimerState({
+                  timeLimit: timeLimit + 60,
+                  speed: speed - 200,
+                });
+              } else {
+                // TODO: check for duplicated title withint the same account before uploading
+                api_art_title({
+                  player: playerState.id,
+                  stage: stage,
+                  art: art,
+                  title: titleRef.current.innerHTML.replace(/^\s+|\s+$/g, ""),
+                });
+                setArtTitle(
+                  stage,
+                  art,
+                  titleRef.current.innerHTML.replace(/^\s+|\s+$/g, "")
+                );
+                setTimerState({
+                  timeLimit: timeLimit + 60,
+                  speed: speed,
+                });
+              }
+            }}
+          >
+            {getArtTitle(stage, art)}
+          </div>
+          <div
+            ref={statementRef}
+            contentEditable={true}
+            suppressContentEditableWarning={true}
+            className={statementEditing ? css.statementInput : css.statement}
+            onFocus={() => {
+              setStatementEditing(true);
+            }}
+            onBlur={() => {
+              setStatementEditing(false);
+              if (
+                getArtStatement(stage, art) === statementRef.current.innerHTML
+              ) {
+                setTimerState({
+                  timeLimit: timeLimit + 600,
+                  speed: speed - 200,
+                });
+              } else {
+                api_art_statement({
+                  player: playerState.id,
+                  stage: stage,
+                  art: art,
+                  statement: statementRef.current.innerHTML,
+                });
+                setArtStatement(stage, art, statementRef.current.innerHTML);
+                setTimerState({
+                  timeLimit: timeLimit + 600,
+                  speed: speed,
+                });
+              }
+            }}
+            dangerouslySetInnerHTML={{ __html: getArtStatement(stage, art) }}
+          />
+        </div>
+      </main>
+    </>
+  );
 }
 
-export function getServerSideProps(context){
-    const { query: { stage, art } } = context;
-    return {
-        props:{
-            stage,
-            art
-        }
-    }
+export function getServerSideProps(context) {
+  const {
+    query: { stage, art },
+  } = context;
+  return {
+    props: {
+      stage,
+      art,
+    },
+  };
 }
